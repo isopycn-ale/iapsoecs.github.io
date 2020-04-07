c4cbcada6c902faf66fc6d87ff9b602e99090148
cdelav3 <33088592+cdelav3@users.noreply.github.com>
2020-04-07 10:17:05 +0200

    Create 2020-04-06-postdoc-ucboulder.m

---
+++ _posts/2020-04-06-postdoc-ucboulder.m
@@ -0,0 +1,17 @@
+---
+layout: post
+title: Postdoc position (2+ years) in Boulder, USA
+subtitle: University of Colorado
+tags: [postdoc, USA, numerical modelling, stochastic parameterization]
+comments: false
+---
+The University of Colorado’s Department of Applied Mathematics (APPM) and NOAA’s Earth System Research Lab, Physical Sciences Division are seeking to fill a postdoctoral research associate position to develop novel stochastic parameterizations for an ocean model (MOM6) and for ocean-atmosphere interactions in NOAA’s Unified Forecast System. The target application is to improve the performance of coupled data assimilation and forecasting for predictions on sub-seasonal-to-seasonal time scales. This position is for at least 2 and possibly 3 years. Your key responsibilities will be
+
+    Collaborate in the development and testing of new ocean stochastic parameterizations and the ocean-atmospheric coupler.
+    Perform software development for exploratory investigation.
+    Conduct and analyze experiments using high-performance computing.
+    Perform pre- and post-processing of data as needed for analysis and visualization.
+    Describe your results in the form of peer-reviewed articles and in oral presentations.
+    
+Further information and the job application link can be found here:
+https://jobs.colorado.edu/jobs/JobDetail/?jobId=24894&emailCampaignId=168&referrer=http://mpowir.org/postdoc-position-ocean-stochastic-parameterization-s2s-forecasting-cu-boulder-noaa/
